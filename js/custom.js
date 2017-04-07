02a317d8624cef2c89c7c84d178a06e467f509e5
Ajithkumar <ajith@gympik.com>
2017-04-07 12:57:41 +0530

    job openings seperate page created

--- js/custom.js
+++ js/custom.js
@@ -1,51 +1,34 @@
 (function ($) {
-    
+
     // Init Wow
-    wow = new WOW( {
+    wow = new WOW({
         animateClass: 'animated',
-        offset:       100
+        offset: 100
     });
     wow.init();
-    
-    // Navigation scrolls
-    $('.navbar-nav li a').bind('click', function(event) {
-        $('.navbar-nav li').removeClass('active');
-        $(this).closest('li').addClass('active');
-        var $anchor = $(this);
-        var nav = $($anchor.attr('href'));
-        if (nav.length) {
-        $('html, body').stop().animate({				
-            scrollTop: $($anchor.attr('href')).offset().top				
-        }, 1500, 'easeInOutExpo');
-        
-        event.preventDefault();
-        }
+
+//hover dropdown menu
+    $('ul.nav li.dropdown').hover(function () {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+    }, function () {
+        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
     });
-    
-    // About section scroll
-    $(".overlay-detail a").on('click', function(event) {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-            scrollTop: $(hash).offset().top
-        }, 900, function(){
-            window.location.hash = hash;
-        });
+    $('.navbar .dropdown > a').click(function () {
+        location.href = this.href;
     });
-    
     //jQuery to collapse the navbar on scroll
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($(".navbar-default").offset().top > 50) {
             $(".navbar-fixed-top").addClass("top-nav-collapse");
         } else {
             $(".navbar-fixed-top").removeClass("top-nav-collapse");
         }
     });
-    
+
     // Testimonials Slider
     $('.bxslider').bxSlider({
-      adaptiveHeight: true,
-      mode: 'fade'
+        adaptiveHeight: true,
+        mode: 'fade'
     });
-    
+
 })(jQuery);
